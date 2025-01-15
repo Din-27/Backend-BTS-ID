1d6b845c0ba4384c49edc42049189ed9a679f54c
Din-27 <herdiyanafirmansyah27@gmail.com>
2025-01-15 16:45:20 +0700

    fix: route file

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const authRouter = require('./auth.route')
+const authRouter = require('./auth.routes')
 const checklistRouter = require('./checklist.routes')
 
 router.use('/', authRouter)
